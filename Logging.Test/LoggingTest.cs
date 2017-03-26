5bece0263612aab536d8df09580836696bb5f6de
Killercodes <debug@live.in>
2017-03-26 23:04:33 +0530

    Initial relasee

---
+++ Logging.Test/LoggingTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Logging.Test
+{
+    [TestClass]
+    public class LoggingTest
+    {
+        [TestMethod]
+        public void VerboseTest ()
+        {
+            TraceLogger c1 = TraceLogger.Setup("verboseTest",0);
+            c1.Verbose("this is verbose");
+            c1.Verbose("this is {0}", "verbose");
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void InfoTest ()
+        {
+            TraceLogger c1 = TraceLogger.Setup("infoTest",0);
+            c1.Info("this is info 1");
+            c1.Info("this is infor {0}", 2);
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void WarnTest ()
+        {
+            TraceLogger c1 = TraceLogger.Setup("WarnTest",0);
+            c1.Warn("this is warning 1");
+            c1.Warn("this is warning {0}", 2);
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void ErrorTest ()
+        {
+            TraceLogger c1 = TraceLogger.Setup("ErrorTest",0);
+            c1.Error("this is ErrorTest 1");
+            c1.Error("this is ErrorTest {0}", 2);
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void CriticalTest ()
+        {
+            TraceLogger c1 = TraceLogger.Setup("ErrorTest");
+            c1.Critical("this is ErrorTest 1");
+            c1.Critical("this is ErrorTest {0}", 2);
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void LogTest ()
+        {
+            TraceLogger c1 = TraceLogger.Setup("ExTest",0);
+
+            try
+            {
+                var v1 = System.IO.File.Open("c:\\hack.dll",System.IO.FileMode.Open);
+            }
+            catch (Exception ex)
+            {
+                c1.LogException(ex);
+                
+            }
+
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void AllTest ()
+        {
+            TraceLogger c1 = TraceLogger.Setup("ExTest",0);
+            
+            c1.Verbose("this is {0}", "verbose");
+            c1.Start("starting logical operation");
+            c1.Verbose("this a test for all and this is verbose output");
+            c1.Verbose("this the same test with info output");
+
+            try
+            {
+                c1.Warn("this might throw an exception");
+                var v1 = System.IO.File.Open("c:\\hack.dll", System.IO.FileMode.Open);
+            }
+            catch (Exception ex)
+            {
+                c1.Error("this is the exception message {0}", ex.Message);
+                c1.LogException(ex);
+                c1.LogException<Exception>(ex);
+            }
+            c1.Stop("Stopping logical operation");
+
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Logging/TraceLogger.cs
@@ -0,0 +1,199 @@
+﻿/*
+ *  TraceLogger
+ *  Application Logger based on TraceSource
+ *  
+ */
+
+
+namespace System.Diagnostics
+{
+    /// <summary>
+    /// Custom logger based on TraceSource
+    /// </summary>
+    public sealed class TraceLogger
+    { 
+        TraceSource _trace_source;
+        int _event_id = 1;
+        private static readonly Random getrandom = new Random();
+        private static readonly object syncLock = new object();
+
+        /// <summary>
+        /// Initialize TraceLogger
+        /// </summary>
+        /// <param name="sourcename">Source name</param>
+        private TraceLogger (string sourcename)
+        {
+            _trace_source = new TraceSource(sourcename,SourceLevels.All);
+            _event_id = generateEventId(); 
+        }
+
+        private TraceLogger (string sourcename, ushort eventId)
+        {
+            _trace_source = new TraceSource(sourcename, SourceLevels.All);
+            _event_id = eventId;
+        }
+
+        /// <summary>
+        /// Setups the trace logger
+        /// </summary>
+        /// <param name="sourcename">the source name </param>
+        /// <param name="eventId">0 will create a new event id randomly, other is just parameter</param>
+        /// <returns></returns>
+        public static TraceLogger Setup (string sourcename, ushort eventId = 0)
+        {
+            if(eventId != 0)
+                return new TraceLogger(sourcename, eventId);
+
+            return new TraceLogger(sourcename);
+        }
+
+        //Generate random _event_id from 1 - 65536
+        private static int generateEventId ()
+        {
+            lock (syncLock)
+            { // synchronize
+                return getrandom.Next(1, 65536);
+            }
+        }
+
+        // log event
+        private void LogEvent (TraceEventType type, string message)
+        {
+            _trace_source.TraceEvent(type, _event_id, message);
+        }
+
+        /// <summary>
+        /// LogException to Trace listeners
+        /// </summary>
+        /// <param name="ex"> exception object</param>
+        public void LogException (Exception ex)
+        {
+            _trace_source.TraceData(TraceEventType.Critical, _event_id, ex);
+        }
+        /// <summary>
+        /// LogException to Trace listeners
+        /// </summary>
+        public void LogException<T> (T ex)
+        {
+            _trace_source.TraceData(TraceEventType.Critical, _event_id, ex);
+        }
+
+        /// <summary>
+        /// Fatal Error or Application crash
+        /// </summary>
+        public void Critical (string format, params object[] args)
+        {
+            LogEvent(TraceEventType.Critical, string.Format(format, args));
+        }
+
+        /// <summary>
+        /// Fatal Error or Application crash
+        /// </summary>
+        public void Critical (string message)
+        {
+            LogEvent(TraceEventType.Critical, message);
+        }
+
+        /// <summary>
+        /// Recoverable error
+        /// </summary>
+        public void Error (string format, params object[] args)
+        {
+            LogEvent(TraceEventType.Error, string.Format(format, args));
+        }
+
+        /// <summary>
+        /// Recoverable error
+        /// </summary>
+        public void Error (string message)
+        {
+            LogEvent(TraceEventType.Error, message);
+        }
+
+        /// <summary>
+        /// Noncritical problem
+        /// </summary>
+        public void Warn (string format, params object[] args)
+        {
+            LogEvent(TraceEventType.Warning, string.Format(format, args));
+        }
+        /// <summary>
+        /// Noncritical problem
+        /// </summary>
+        public void Warn (string message)
+        {
+            LogEvent(TraceEventType.Warning,message);
+        }
+
+        /// <summary>
+        /// Informational Message
+        /// </summary>
+        public void Info (string format, params object[] args)
+        {
+            LogEvent(TraceEventType.Information, string.Format(format, args));
+        }
+        /// <summary>
+        /// Informational Message
+        /// </summary>
+        public void Info (string message)
+        {
+            LogEvent(TraceEventType.Information,message);
+        }
+
+        /// <summary>
+        /// Debugging Trace
+        /// </summary>
+        public void Verbose (string format, params object[] args)
+        {
+            LogEvent(TraceEventType.Verbose, string.Format(format, args));
+        }
+
+        /// <summary>
+        /// Debugging Trace
+        /// </summary>
+        public void Verbose (string message)
+        {
+            LogEvent(TraceEventType.Verbose, message);
+        }
+
+        /// <summary>
+        /// Starting a logical operation
+        /// </summary>
+        public void Start (string message)
+        {
+            LogEvent(TraceEventType.Start, message);
+        }
+
+        /// <summary>
+        /// Stopping a logical operation
+        /// </summary>
+        public void Stop (string message)
+        {
+            LogEvent(TraceEventType.Stop, message);
+        }
+
+        /// <summary>
+        /// Resumption a logical operation
+        /// </summary>
+        public void Resume (string message)
+        {
+            LogEvent(TraceEventType.Resume, message);
+        }
+
+        /// <summary>
+        /// Suspension a logical operation
+        /// </summary>
+        public void Suspend (string message)
+        {
+            LogEvent(TraceEventType.Suspend, message);
+        }
+
+        /// <summary>
+        /// Changing of corelation identity
+        /// </summary>
+        public void Transfer (string message)
+        {
+            LogEvent(TraceEventType.Transfer, message);
+        }
+    }
+}
